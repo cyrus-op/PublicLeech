7678210abb5cc83bda7934328cd034da480c91c3
cyrus-op <76032422+cyrus-op@users.noreply.github.com>
2021-01-27 04:37:51 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -28,7 +28,7 @@ class Config:
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = get_config(
         "DEF_THUMB_NAIL_VID_S",
-        "https://telegra.ph/file/8b973b270f4f380a427b1.png"
+        "https://telegra.ph/file/a0cc241f2006b7c294ef2.jpg"
     )
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = int(get_config(
