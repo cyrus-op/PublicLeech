a5573fc311eb84d49be9a651923292c60a79cb76
cyrus-op <76032422+cyrus-op@users.noreply.github.com>
2021-01-27 04:59:27 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,90 +1,19 @@
-from tobrot.get_cfg import get_config
+from torbot.sample_config import Config
 
+#read readme too before filling these stuffs
 
-class Config:
-    # get a token from @BotFather
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", 1442173461:AAH82dvwnid7d1nC7jVJ9-1sQARKEgA3W9I)
-    # The Telegram API things
-    APP_ID = int(get_config("APP_ID", 2526751))
-    API_HASH = get_config("API_HASH", 9d95cd9e9bede6332e1da19276d9ad63)
-    # Get these values from my.telegram.org
-    # array to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = set(
-        int(x) for x in get_config(
-            "AUTH_CHANNEL",
-            -1001468189721
-        ).split()
-    )
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "./DOWNLOADS")
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = int(get_config("MAX_FILE_SIZE", 50000000))
-    TG_MAX_FILE_SIZE = int(get_config("TG_MAX_FILE_SIZE", 2097152000))
-    FREE_USER_MAX_FILE_SIZE = int(
-        get_config("FREE_USER_MAX_FILE_SIZE", 50000000)
-    )
-    # chunk size that should be used with requests
-    CHUNK_SIZE = int(get_config("CHUNK_SIZE", 128))
-    # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = get_config(
-        "DEF_THUMB_NAIL_VID_S",
-        "https://telegra.ph/file/a0cc241f2006b7c294ef2.jpg"
-    )
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = int(get_config(
-        "MAX_MESSAGE_LENGTH",
-        4096
-    ))
-    # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = int(get_config(
-        "PROCESS_MAX_TIMEOUT",
-        3600
-    ))
-    #
-    ARIA_TWO_STARTED_PORT = int(get_config(
-        "ARIA_TWO_STARTED_PORT",
-        6800
-    ))
-    EDIT_SLEEP_TIME_OUT = int(get_config(
-        "EDIT_SLEEP_TIME_OUT",
-        1
-    ))
-    MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(get_config(
-        "MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START",
-        600
-    ))
-    MAX_TG_SPLIT_FILE_SIZE = int(get_config(
-        "MAX_TG_SPLIT_FILE_SIZE",
-        1900000000
-    ))
-    # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "█")
-    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "░")
-    # add offensive API
-    TG_OFFENSIVE_API = get_config("TG_OFFENSIVE_API", None)
-    # URL for the rclone configuration
-    R_CLONE_CONF_URI = get_config("R_CLONE_CONF_URI", None)
-    # Destination folder for the rclone
-    R_CLONE_DEST = get_config("R_CLONE_DEST", "/PublicLeech")
-    # because, https://t.me/c/1494623325/5603
-    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
-    #
-    LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "PublicLeech.log")
-    #
-    SP_LIT_ALGO_RITH_M = get_config(
-        "SP_LIT_ALGO_RITH_M",
-        "hjs"
-    )
-    #
-    DIS_ABLE_ST_GFC_COMMAND_I = get_config(
-        "DIS_ABLE_ST_GFC_COMMAND_I",
-        False
-    )
-    # array to store the users who will have control (permissions)
-    # in the bot
-    SUDO_USERS = set(
-        int(x) for x in get_config(
-            "SUDO_USERS",
-            should_prompt=True
-        ).split()
-    )
+class Config(Config):
+	TG_BOT_TOKEN= "1442173461:AAH82dvwnid7d1nC7jVJ9-1sQARKEgA3W9I" #imp
+	APP_ID = 2526751 #imp
+	API_HASH = "9d95cd9e9bede6332e1da19276d9ad63" #imp
+	AUTH CHANNEL = [-1001468189721, 1197580900] #imp replace your_id from telegram or delete
+	INDEX_LINK = ""
+	GLEECH_COMMAND = "gleech"
+	YTDL_COMMAND = "ytdl"
+	TELEGRAM_LEECH_COMMAND_G = "tleech"
+	CLONE_COMMAND_G = "gclone"
+	PYTDL_COMMAND_G = "pytdl"
+	DESTINATION_FOLDER = "Gdrive bot"
+	LEECH_COMMAND = "leech"
+	RCLONE_CONFIG = """""
+	#put your config (replacing new lines with \n) in triple quote like above: """<your one liner config>"""
